4b5e0567f4fdda01a54e3ca51c8677f839e52e29
ranimyahyaoui <ranim.yahyaoui@supcom.ucar.tn>
2026-06-26 01:27:31 +0100

    fix: dotenv

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
-require("dotenv").config();
-
+import dotenv from "dotenv";
+dotenv.config();
 const express=require('express');
 const cors=require('cors');
 import analyzeRouter from "./routes/analyze.js";
